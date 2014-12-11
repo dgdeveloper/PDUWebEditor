129c2d8e6f84b94f8b3bd25ef76ee1e4b5b7785e
david <davidguo.developer@outlook.com>
2014-12-10 16:04:21 -0800

    refactor DAL

---
+++ PDU Web Editor/PDU Web Editor/DAL/Infrastructure/SqlUnitOfWork.cs
@@ -0,0 +1,92 @@
+﻿using PDU_Web_Editor.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PDU_Web_Editor.DAL
+{
+    public class SqlUnitOfWork:IUnitOfWork
+    {
+        private DbContext _context;
+        private IRepository<Asset> _assetRepository;
+        private IRepository<PDU> _pduRepository;
+        private IRepository<Record> _RecordRepostiory;
+        private bool disposed = false;
+
+        public SqlUnitOfWork(DbContext context)
+        {
+            this._context = context;
+        }
+
+        #region IUnitOfWork Member
+        public IRepository<Asset> AssetRepository
+        {
+            get
+            {
+                if (this._assetRepository == null)
+                {
+                    this._assetRepository = new DbRepositoryBase<Asset>(_context);
+                }
+                return _assetRepository;
+            }
+
+        }
+
+        public IRepository<Record> RecordRepostiory
+        {
+            get
+            {
+                if (this._RecordRepostiory == null)
+                {
+                    this._RecordRepostiory = new DbRepositoryBase<Record>(_context);
+                }
+                return _RecordRepostiory;
+            }
+            
+        }
+
+        public IRepository<PDU> PDURepository
+        {
+            get
+            {
+                if (this._pduRepository == null)
+                {
+                    this._pduRepository = new DbRepositoryBase<PDU>(_context);
+                }
+                return _pduRepository;
+            }
+            
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        #endregion
+
+        #region IDisposable Member
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+      
+    }
+}
+
